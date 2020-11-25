f6f0711f139995e2762512ea78a816a290cc169c
amanahazzahra <amanahazzahra@gmail.com>
2020-11-25 15:26:49 +0800

    tugas

--- app/src/main/java/com/zinni/hewanpedia/GaleryActivity.java
+++ app/src/main/java/com/zinni/hewanpedia/GaleryActivity.java
@@ -1,13 +1,112 @@
 package com.zinni.hewanpedia;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.zinni.hewanpedia.model.hewan;
+
+import java.util.List;
+
+
 
 public class GaleryActivity extends AppCompatActivity {
 
+    List<hewan> hewans;
+    int indeksTampil = 0;
+    String jenisHewan;
+    Button btnPertama,btnTerakhir,btnSebelumnya,btnBerikutnya;
+    TextView txJenis,txAsal,txDeskripsi,txJudul;
+    ImageView ivFotoHewan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_galery);
+        Intent intent = getIntent();
+        jenisHewan = intent.getStringExtra(MainActivity.JENIS_GALERI_KEY);
+        hewans = DataProvider.getHewansByTipe(this,jenisHewan);
+        inisialisasiView();
+        tampilkanProfil();
+    }
+    private void inisialisasiView() {
+        btnPertama = findViewById(R.id.btnPertama);
+        btnSebelumnya = findViewById(R.id.btnSebelumnya);
+        btnBerikutnya = findViewById(R.id.btnBerikutnya);
+        btnTerakhir = findViewById(R.id.btnTerakhir);
+
+        btnPertama.setOnClickListener(view -> hewanPertama());
+        btnTerakhir.setOnClickListener(view -> hewanTerakhir());
+        btnSebelumnya.setOnClickListener(view -> hewanSebelumnya());
+        btnBerikutnya.setOnClickListener(view -> hewanBerikutnya());
+
+        txJenis = findViewById(R.id.txJenis);
+        txAsal = findViewById(R.id.txAsal);
+        txDeskripsi = findViewById(R.id.txDeskripsi);
+        ivFotoHewan = findViewById(R.id.gambarHewan);
+
+        txJudul = findViewById(R.id.txJudul);
+        txJudul.setText("Berbagai Macam Ras "+jenisHewan);
+    }
+
+
+    private void tampilkanProfil() {
+        hewan k = hewans.get(indeksTampil);
+        Log.d("ANJING","Menampilkan anjing "+k.getJenis());
+        txJenis.setText(k.getJenis());
+        txAsal.setText(k.getAsal());
+        txDeskripsi.setText(k.getDeskripsi());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            ivFotoHewan.setImageDrawable(this.getDrawable(k.getDrawableRes()));
+        }
+    }
+
+    private void hewanPertama() {
+        int posAwal = 0;
+        if (indeksTampil == posAwal) {
+            Toast.makeText(this,"Sudah di posisi pertama",Toast.LENGTH_SHORT).show();
+            return;
+        } else {
+            indeksTampil = posAwal;
+            tampilkanProfil();
+        }
+    }
+
+    private void hewanTerakhir() {
+        int posAkhir = hewans.size() - 1;
+        if (indeksTampil == posAkhir) {
+            Toast.makeText(this,"Sudah di posisi terakhir",Toast.LENGTH_SHORT).show();
+            return;
+        } else {
+            indeksTampil = posAkhir;
+            tampilkanProfil();
+        }
+    }
+
+    private void hewanBerikutnya() {
+        if (indeksTampil == hewans.size() - 1) {
+            Toast.makeText(this,"Sudah di posisi terakhir",Toast.LENGTH_SHORT).show();
+            return;
+        } else {
+            indeksTampil++;
+            tampilkanProfil();
+        }
+    }
+
+    private void hewanSebelumnya() {
+        if (indeksTampil == 0) {
+            Toast.makeText(this,"Sudah di posisi pertama",Toast.LENGTH_SHORT).show();
+            return;
+        } else {
+            indeksTampil--;
+            tampilkanProfil();
+        }
     }
 }
+
